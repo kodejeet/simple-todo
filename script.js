3c1a6756d9c1c06670f5bcb92f1839407176484b
kodejeet <roshansahx2003@gmail.com>
2025-02-23 04:59:05 +0530

    fixed alert box error

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
 function addTask(){
-    if (inputBox === ''){
+    if (inputBox.value === ''){
         alert("You must write something..");
     }
     else{
         
         let li = document.createElement("li");
         li.innerHTML= inputBox.value;
-        listContainer = appendChild(li);
+        listContainer.appendChild(li);
     }
 }
